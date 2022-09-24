aaa9d7742912111263361889d31699aae7168427
Nikki <thanwinnikki@gmail.com>
2022-09-25 04:01:17 +0800

    Add testing for Sign Up page.

--- frontend/src/tests/Sign-Up-Page.test.js
+++ frontend/src/tests/Sign-Up-Page.test.js
@@ -15,3 +15,71 @@ it("renders sign in correctly", () => {
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test("renders create account header correctly", () => {
+  render(
+    <Router location={"/"}>
+      <Routes>
+        <Route path="/" element={<SignUpPage />} />
+      </Routes>
+    </Router>,
+  );
+
+  expect(
+    screen.getByRole("heading", {
+      name: /create account/i,
+    }),
+  ).toHaveTextContent("Create Account");
+});
+
+test("renders username textbox correctly", () => {
+  render(
+    <Router location={"/"}>
+      <Routes>
+        <Route path="/" element={<SignUpPage />} />
+      </Routes>
+    </Router>,
+  );
+
+  expect(
+    screen.getByRole("textbox", {
+      name: /username/i,
+    }),
+  ).toBeInTheDocument();
+});
+
+test("renders create account button correctly", () => {
+  render(
+    <Router location={"/"}>
+      <Routes>
+        <Route path="/" element={<SignUpPage />} />
+      </Routes>
+    </Router>,
+  );
+
+  expect(
+    screen.getByRole("button", {
+      name: /Create Account/i,
+    }),
+  ).toBeInTheDocument();
+
+  screen.logTestingPlaygroundURL();
+});
+
+test("renders back button correctly", () => {
+  render(
+    <Router location={"/"}>
+      <Routes>
+        <Route path="/" element={<SignUpPage />} />
+      </Routes>
+    </Router>,
+  );
+
+  expect(
+    screen.getByRole("button", {
+      name: "",
+    }),
+  ).toBeInTheDocument();
+
+  screen.logTestingPlaygroundURL();
+});
