6e28a2613abf87fbc71e5f22ba1449503e981633
Lye Yi Xian <lyeyx999@hotmail.com>
2022-10-05 14:53:53 +0800

    Change user-service bcrypt package to bcryptjs (Docker cant user bcrypt)

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,62 +1,91 @@
-import { ormCreateUser as _createUser, ormUserExists as _userExists, ormUpdatePassword as _updatePassword, ormDeleteUser as _deleteUser } from '../model/user-orm.js'
-import { SALT_ROUNDS, STATUS_CODE_CREATED, STATUS_CODE_BAD_REQUEST, STATUS_CODE_SERVER_ERROR } from "../common/constants.js";
-import bcrypt from 'bcrypt';
+import {
+  ormCreateUser as _createUser,
+  ormUserExists as _userExists,
+  ormUpdatePassword as _updatePassword,
+  ormDeleteUser as _deleteUser,
+} from '../model/user-orm.js'
+import {
+  SALT_ROUNDS,
+  STATUS_CODE_CREATED,
+  STATUS_CODE_BAD_REQUEST,
+  STATUS_CODE_SERVER_ERROR,
+} from '../common/constants.js'
+import bcrypt from 'bcryptjs'
 export async function createUser(req, res) {
-    try {
-        const { username, password } = req.body;
-        const userExists = await _userExists(username);
-        if (username && password && !userExists) {
+  try {
+    const { username, password } = req.body
+    const userExists = await _userExists(username)
+    if (username && password && !userExists) {
+      const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+      const resp = await _createUser(username, hashedPassword)
 
-            const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-            const resp = await _createUser(username, hashedPassword);
-
-            if (resp.err) {
-                return res.status(STATUS_CODE_BAD_REQUEST).json({message: 'Could not create a new user!'});
-            } else {
-                console.log(`Created new user ${username} successfully!`)
-                return res.status(STATUS_CODE_CREATED).json({message: `Created new user ${username} successfully!`});
-            }
-        } else if (!username || !password) {
-            return res.status(STATUS_CODE_BAD_REQUEST).json({message: 'Username and/or Password are missing!'});
-        } else if (userExists) {
-            console.log("Usernmae exists. Cannot create account")
-            return res.status(STATUS_CODE_BAD_REQUEST).json({message: 'Username is already used!'})
-        } else {
-            return res.status(STATUS_CODE_BAD_REQUEST).json({message: 'System failed to create new user!'})
-        }
-    } catch (err) {
-        return res.status(STATUS_CODE_SERVER_ERROR).json({message: 'Database failure when creating new user!'})
+      if (resp.err) {
+        return res
+          .status(STATUS_CODE_BAD_REQUEST)
+          .json({ message: 'Could not create a new user!' })
+      } else {
+        console.log(`Created new user ${username} successfully!`)
+        return res
+          .status(STATUS_CODE_CREATED)
+          .json({ message: `Created new user ${username} successfully!` })
+      }
+    } else if (!username || !password) {
+      return res
+        .status(STATUS_CODE_BAD_REQUEST)
+        .json({ message: 'Username and/or Password are missing!' })
+    } else if (userExists) {
+      console.log('Usernmae exists. Cannot create account')
+      return res
+        .status(STATUS_CODE_BAD_REQUEST)
+        .json({ message: 'Username is already used!' })
+    } else {
+      return res
+        .status(STATUS_CODE_BAD_REQUEST)
+        .json({ message: 'System failed to create new user!' })
     }
+  } catch (err) {
+    return res
+      .status(STATUS_CODE_SERVER_ERROR)
+      .json({ message: 'Database failure when creating new user!' })
+  }
 }
 
 export async function changePassword(req, res) {
-    try {
-        const { password } = req.body;
-        const userId = req.userId;
-        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-        const resp = await _updatePassword(userId, hashedPassword);
+  try {
+    const { password } = req.body
+    const userId = req.userId
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+    const resp = await _updatePassword(userId, hashedPassword)
 
-        if (resp.err) {
-            return res.status(STATUS_CODE_BAD_REQUEST).json({message: 'Could not update password!'});
-        } else {
-            console.log(`Changed ${userId}'s password successfully!`);
-            return res.status(STATUS_CODE_CREATED).json({message: 'Password changed successfully!'});
-        }
-
-    } catch (err) {
-        console.log(err);
-        return res.status(STATUS_CODE_SERVER_ERROR).json({message: 'Server errror, failed to change password!'});
+    if (resp.err) {
+      return res
+        .status(STATUS_CODE_BAD_REQUEST)
+        .json({ message: 'Could not update password!' })
+    } else {
+      console.log(`Changed ${userId}'s password successfully!`)
+      return res
+        .status(STATUS_CODE_CREATED)
+        .json({ message: 'Password changed successfully!' })
     }
+  } catch (err) {
+    console.log(err)
+    return res
+      .status(STATUS_CODE_SERVER_ERROR)
+      .json({ message: 'Server errror, failed to change password!' })
+  }
 }
 
 export async function deleteUser(req, res) {
-    try {
-        const userId  = req.userId;
-        const user = await _deleteUser(userId);
-        console.log(`${userId} has been deleted`);
-        return res.status(STATUS_CODE_CREATED ).json({message: 'User deleted successfully!'});
-
-    } catch (err) {
-        return res.status(STATUS_CODE_SERVER_ERROR).json({message: 'Database failure when deleting user!'});
-    }
+  try {
+    const userId = req.userId
+    const user = await _deleteUser(userId)
+    console.log(`${userId} has been deleted`)
+    return res
+      .status(STATUS_CODE_CREATED)
+      .json({ message: 'User deleted successfully!' })
+  } catch (err) {
+    return res
+      .status(STATUS_CODE_SERVER_ERROR)
+      .json({ message: 'Database failure when deleting user!' })
+  }
 }
