34b7d304e5f2bc85c50ec2255532c24635af77d5
Lye Yi Xian <lyeyx999@hotmail.com>
2022-10-08 22:22:56 +0800

    Refactor collaboration service

--- collaboration-service/config/cache.js
+++ collaboration-service/config/cache.js
@@ -7,7 +7,7 @@ const redisClient = createClient({
 const connectCache = async () => {
   try {
     await redisClient.connect()
-    await redisClient.flushAll()
+    await redisClient.flushAll() // TODO: need to check if this is correct in distributed system
     redisClient.on('error', console.error)
     console.log('Connected to redis locally!')
   } catch (error) {
