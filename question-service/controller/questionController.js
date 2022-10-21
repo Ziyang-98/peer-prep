5d8d30be81a537efd6376a0b84b5110209471b1e
Lye Yi Xian <lyeyx999@hotmail.com>
2022-10-21 21:52:32 +0800

    Add testing endpoint for populating db

--- question-service/controller/questionController.js
+++ question-service/controller/questionController.js
@@ -24,8 +24,59 @@ const getRandomQuestionOfDifficulty = asyncHandler(async (req, res) => {
 
   res.status(200).json(problem)
 })
+
+// Description: Populate database with problems (nor for public use)
+// Route: POST /api/questionService/question/test
+// Access: Only in development by devs
+const test = asyncHandler(async (req, res) => {
+  const problemList = await leetcode.problems({ limit: 1 })
+  const totalQuestions = 100 || problemList.total // TODO: change to other number later
+  let offset = 0
+  const limit = 10
+  const problemTitlePromises = []
+
+  while (offset < totalQuestions) {
+    console.log('in while -->', offset)
+    problemTitlePromises.push(leetcode.problems({ limit, offset }))
+    offset += limit
+  }
+
+  const problemTitles = await Promise.all(problemTitlePromises)
+  const problemPromises = []
+
+  for (const problemTitle of problemTitles) {
+    for (const question of problemTitle.questions) {
+      if (!question.isPaidOnly) {
+        console.log('slug -->', question.titleSlug)
+        problemPromises.push(leetcode.problem(question.titleSlug))
+      }
+    }
+  }
+
+  console.log('done -->')
+  const problems = await Promise.all(problemPromises)
+  const newProblems = problems.map((problem) => ({
+    questionId: problem.questionId,
+    questionFrontendId: problem.questionFrontendId,
+    title: problem.title,
+    titleSlug: problem.titleSlug,
+    content: problem.content,
+    difficulty: problem.difficulty,
+    likes: problem.likes,
+    dislikes: problem.dislikes,
+    similarQuestions: problem.similarQuestions,
+    topicTags: problem.topicTags,
+    codeSnippets: problem.codeSnippets,
+    stats: problem.stats,
+    hints: problem.hints,
+    sampleTestCase: problem.sampleTestCase,
+  }))
+
+  await Problem.insertMany(newProblems)
+  res.json({ message: `Done populating ${newProblems.length} questions` })
 })
 
 module.exports = {
   getRandomQuestionOfDifficulty,
+  test,
 }
--- question-service/routes/questionRoutes.js
+++ question-service/routes/questionRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const {
   getRandomQuestionOfDifficulty,
+  test,
 } = require('../controller/questionController')
 
 const router = express.Router()
 
 router.get('/random/:roomId', getRandomQuestionOfDifficulty)
+router.post('/test', test)
 
 module.exports = router
