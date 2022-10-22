462f8aee192584cfcca0fbf68fc714cdd5a57059
jaredlhf <jlhfqaz@gmail.com>
2022-10-22 23:31:45 +0800

    try to fix cors issue

--- collaboration-service/index.js
+++ collaboration-service/index.js
@@ -14,6 +14,11 @@ app.use(express.json())
 app.use(cors())
 app.options('*', cors())
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 // Routes
 app.get('/', (req, res) => {
