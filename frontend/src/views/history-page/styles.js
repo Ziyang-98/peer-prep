5118178c853c62a7eca547248cd4b791551653a6
Lye Yi Xian <lyeyx999@hotmail.com>
2022-11-01 16:47:48 +0800

    Install date-fns. Display the duration ago. Add styling.

--- frontend/src/views/history-page/styles.js
+++ frontend/src/views/history-page/styles.js
@@ -1,17 +1,25 @@
 export const styles = {
   paper: {
     bgcolor: "primary.light",
-    width: "50%",
-    marginLeft: "50%",
-    marginRight: "50%",
+    width: "100%",
+    marginLeft: "25%",
+    marginRight: "25%",
   },
   box: {
     display: "flex",
-    justifyConent: "center",
+    justifyContent: "center",
     alignItems: "center",
     height: "100vh",
   },
   questionTitle: {
     marginLeft: "50px",
+  },
+  duration: {
+    color: "rgb(105 108 112 / 60%)",
+    textAlign: "right",
+  },
+  listItemButton: {
+    display: "flex",
+    justifyContent: "space-between",
   },
 };
