1ed944b124ccc50a6bc35d9a1d826d0c2fc59dbb
Lim Zi Yang <e0406401@u.nus.edu>
2022-11-05 00:23:29 +0800

    update history page

--- frontend/src/views/history-page/index.js
+++ frontend/src/views/history-page/index.js
@@ -1,31 +1,44 @@
-import { Box, ListItemButton, ListItemText, Paper } from "@mui/material";
+import {
+  Box,
+  ListItemButton,
+  ListItemText,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { formatDistance } from "date-fns";
 import { styles } from "./styles";
 import useHistory from "hooks/useHistory";
 
 const HistoryPage = () => {
   const { handleQuestionClick, pastQuestions } = useHistory();
-
   return (
     <Box sx={styles.box}>
-      <Paper elevation={3} sx={styles.paper}>
-        {pastQuestions.map((question) => (
-          <ListItemButton
-            sx={styles.listItemButton}
-            component="a"
-            key={question.titleSlug}
-            onClick={() => handleQuestionClick(question.titleSlug)}
-          >
-            <ListItemText primary={question.title} />
-            <ListItemText
-              sx={styles.duration}
-              primary={formatDistance(question.createdAt, new Date(), {
-                addSuffix: true,
-              })}
-            />
-          </ListItemButton>
-        ))}
-      </Paper>
+      <Box sx={styles.historyContainer}>
+        <Typography variant="h4">Past Completed Questions:</Typography>
+        <Paper elevation={3} sx={styles.paper}>
+          {pastQuestions.length === 0 && (
+            <Box sx={styles.noQuestionTextHolder}>
+              <Typography variant="h5">No questions attempted yet</Typography>
+            </Box>
+          )}
+          {pastQuestions.map((question, index) => (
+            <ListItemButton
+              sx={styles.listItemButton}
+              component="a"
+              key={question.titleSlug}
+              onClick={() => handleQuestionClick(question.titleSlug)}
+            >
+              <ListItemText primary={question.title} />
+              <ListItemText
+                sx={styles.duration}
+                primary={formatDistance(question.createdAt, new Date(), {
+                  addSuffix: true,
+                })}
+              />
+            </ListItemButton>
+          ))}
+        </Paper>
+      </Box>
     </Box>
   );
 };
