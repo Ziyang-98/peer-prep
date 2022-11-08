df99f123fe297d63ccab05261b09e1504f873bff
Lim Zi Yang <e0406401@u.nus.edu>
2022-11-08 22:06:31 +0800

    update styles of solo room

--- frontend/src/views/solo-room-page/styles.js
+++ frontend/src/views/solo-room-page/styles.js
@@ -8,16 +8,18 @@ export const styles = {
     height: "90vh",
   },
   bottomActionHolder: {
-    width: "90%",
+    width: "100%",
     display: "flex",
     flexDirection: "row-reverse",
     justifyContent: "space-between",
     alignItems: "center",
     mt: "20px",
     position: "relative",
+    bgcolor: "secondary.light",
   },
   returnButton: {
     height: "40px",
+    mr: "5%",
     bgcolor: "primary.mainColor",
     "&:hover": {
       bgcolor: "primary.dark",
